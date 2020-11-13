b14258b01c4f6e9cd3a71150e9169e3782db0950
zoujr <18870680299@163.com>
2020-11-13 13:10:10 +0800

    fixed MEM_EN

--- src/main/scala/LC3/DataPath.scala
+++ src/main/scala/LC3/DataPath.scala
@@ -135,12 +135,14 @@ class DataPath extends Module {
   /*********** Memory ****************/
 
   // address control logic that convered by truth table
-  val MEM_EN = SIG.MIO_EN && SIG.R_W
+  val MEM_RD = SIG.MIO_EN && !SIG.R_W
+  val MEM_EN = SIG.MIO_EN && (MAR < 0xfe00.U)
+
   val IN_MUX = MuxCase(io.mem.rdata, Array(
-    (MEM_EN && (MAR === 0xfe00.U)) -> KBSR,//KBSR,
-    (MEM_EN && (MAR === 0xfe02.U)) -> KBDR,//KBDR,
-    (MEM_EN && (MAR === 0xfe04.U)) -> io.mem.rdata,//DSR,
-    (MEM_EN && (MAR < 0xfe00.U)) -> io.mem.rdata
+    (MEM_RD && (MAR === 0xfe00.U)) -> KBSR,
+    (MEM_RD && (MAR === 0xfe02.U)) -> KBDR,
+    (MEM_RD && (MAR === 0xfe04.U)) -> io.mem.rdata, // DSR
+    (MEM_EN && !SIG.R_W) -> io.mem.rdata
     ))
 
   // UART Input
