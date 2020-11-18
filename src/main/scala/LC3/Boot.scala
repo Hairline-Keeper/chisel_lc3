9136750c84e975066eae42e027150a827a52b1b8
Zhangfw <471348957@qq.com>
2020-11-18 20:14:11 +0800

    add mem initial

---
+++ src/main/scala/LC3/Boot.scala
@@ -0,0 +1,55 @@
+package LC3
+
+import chisel3._
+import chisel3.util._
+
+class Boot extends Module {
+  val io = IO(new Bundle() {
+    val uart_rxd = Input(Bool())
+
+    val work    = Output(Bool())
+    val initPC  = ValidIO(UInt(16.W))
+    val initMem = Flipped(new MemIO)
+  })
+
+  val uartRecv = Module(new UartRecv)
+  val uartData = uartRecv.io.uart_data
+  val uartDone = uartRecv.io.uart_done
+  uartRecv.io.sys_clk := clock
+  uartRecv.io.sys_rst_n := reset.asBool()
+  uartRecv.io.uart_rxd := io.uart_rxd
+
+  val initpc :: initmem :: work :: Nil = Enum(3)
+  val lc3State = RegInit(initpc)
+  when(reset.asBool()){ lc3State := initpc }
+
+  val memAddr = Reg(UInt(16.W))
+  val second    = RegInit(init = false.B)     // receive uart data twice and connect the data to 16bits
+  val firstData = RegNext(uartData, uartDone)
+  val fullData  = Cat(uartData, firstData)    // (secondData, firstData)
+  val fullDond  = second && uartDone
+
+  when(uartDone){ second := !second }
+
+  when(lc3State === initpc && fullDond){
+    memAddr := fullData
+    lc3State := initmem
+  }
+
+  when(lc3State === initmem && fullDond){
+    memAddr := memAddr + 1.U
+    when(uartData === 0x60.U){
+      lc3State := work
+    }
+  }
+
+  io.initPC.valid := (lc3State === initpc) && fullDond
+  io.initPC.bits := fullData
+
+  io.initMem <> DontCare
+  io.initMem.wen := (lc3State === initmem) && fullDond
+  io.initMem.wdata := fullData
+  io.initMem.waddr := memAddr
+
+  io.work := (lc3State === work)
+}
--- src/main/scala/LC3/Top.scala
+++ src/main/scala/LC3/Top.scala
@@ -7,61 +7,21 @@ import chisel3.experimental._
 class Top extends Module {
   val io = IO(new UARTBundle)
 
-  val setpc :: init :: work :: Nil = Enum(3)
-  val lc3State = RegInit(setpc)
-  when(reset.asBool()){ lc3State := setpc }
-
-  val pos = RegInit(false.B)
-
-  val PC = Reg(UInt(16.W))
-
-  val offset = RegInit(0.U)
-  val lowData = Reg(UInt(8.W))
-
+  val boot = Module(new Boot)
   val controller = Module(new Controller)
   val dataPath = Module(new DataPath)
   val memory = Module(new Memory)
 
   controller.io.in <> dataPath.io.out
-  controller.io.work := (lc3State === work)
+  controller.io.work := boot.io.work
 
   dataPath.io.signal <> controller.io.out
-  dataPath.io.initPC := PC
+  dataPath.io.initPC := boot.io.initPC.bits
 
   memory.io <> dataPath.io.mem
+  memory.io <> boot.io.initMem
 
-  val uartRecv = Module(new UartRecv)
-  val uartData = uartRecv.io.uart_data
-  val uartDone = uartRecv.io.uart_done
-  uartRecv.io.sys_clk := clock
-  uartRecv.io.sys_rst_n := reset
-  uartRecv.io.uart_rxd := io.uart_rxd
-
-  
-  when(lc3State === setpc){
-    when(uartDone){
-      pos := !pos
-      lowData := Mux(pos, lowData, uartData)
-      when(pos){
-        lc3State := init
-        PC := Cat(uartData, lowData)
-      }
-    }
-  }
-
-  when(lc3State === init && uartDone === true.B){
-    pos := !pos
-    lowData := Mux(pos, lowData, uartData)
-    memory.io.wen := pos //&& uartDone
-    memory.io.wdata := Cat(uartData, lowData)
-    memory.io.waddr := PC + offset
-    offset := Mux(pos, offset + 1.U, offset)
-    when(uartData === 0x60.U){
-      lc3State := work
-    }
-  }
-
-
+  boot.io.uart_rxd := io.uart_rxd
   io.uart_txd := io.uart_rxd
 
   dontTouch(controller.io)
